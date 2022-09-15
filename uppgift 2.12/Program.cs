3a512ec1dde5599388b81c1c7c3888590e3d99f1
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-15 13:33:05 +0200

    I detta program jag har gjort så den räknar hyra på en bil. Den räknar varje dag som går och hur många kilometer man kört Add project files.

---
+++ uppgift 2.12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgift_2._12
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int Startavgift = 300;
+            Console.WriteLine("Hur många dagar ska du köra?");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många kilometer ska du köra bilen?");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Startavgift + x * 500 - 500 + y ); // Här är matten för räkningen.
+            
+
+
+        }
+
+    }
+}           
